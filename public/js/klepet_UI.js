2f129ff41ab082c0e976ce7b73cb72b79d6365a0
Miha Jamsek <miha_jamsek@windowslive.com>
2016-04-09 20:24:12 +0000

    vmes

--- public/js/klepet_UI.js
+++ public/js/klepet_UI.js
@@ -1,196 +1,155 @@
-    function divElementEnostavniTekst(sporocilo) {
-      var jeSmesko = sporocilo.indexOf('http://sandbox.lavbic.net/teaching/OIS/gradivo/') > -1;
-      if (jeSmesko) {
-        sporocilo = sporocilo.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace('&lt;img', '<img').replace('png\' /&gt;', 'png\' />');
-        return $('<div style="font-weight: bold"></div>').html(sporocilo);
-      } else {
-        return $('<div style="font-weight: bold;"></div>').text(sporocilo);
+  function divElementEnostavniTekst(sporocilo) {
+    var jeSmesko = sporocilo.indexOf('http://sandbox.lavbic.net/teaching/OIS/gradivo/') > -1;
+    if (jeSmesko) {
+      sporocilo = sporocilo.replace(/\</g, '&lt;').replace(/\>/g, '&gt;').replace('&lt;img', '<img').replace('png\' /&gt;', 'png\' />');
+      return $('<div style="font-weight: bold"></div>').html(sporocilo);
+    } else {
+      return $('<div style="font-weight: bold;"></div>').text(sporocilo);
+    }
+  }
+  
+  function divElementHtmlTekst(sporocilo) {
+    return $('<div></div>').html('<i>' + sporocilo + '</i>');
+  }
+  
+  function procesirajVnosUporabnika(klepetApp, socket) {
+    var sporocilo = $('#poslji-sporocilo').val();
+    sporocilo = dodajSmeske(sporocilo);
+    var sistemskoSporocilo;
+  
+    if (sporocilo.charAt(0) == '/') {
+      sistemskoSporocilo = klepetApp.procesirajUkaz(sporocilo);
+      if (sistemskoSporocilo) {
+        $('#sporocila').append(divElementHtmlTekst(sistemskoSporocilo));
       }
-    }
-    function divElementDodajSliko(link) {
-    	return $('<div></div>').html('<img src="' + link + '" width="200" hspace="20">');
-    }
-
-    
-    function divElementHtmlTekst(sporocilo) {
-      return $('<div></div>').html('<i>' + sporocilo + '</i>');
-    }
-    
-    function jeSlika(sporocilo){
-      if ((sporocilo.startsWith('http://') || sporocilo.startsWith('https://'))
-    			&& (sporocilo.endsWith('jpg') || sporocilo.endsWith('png') || sporocilo.endsWith('gif') )){
-    			  return true;
-    			}
-    			return false;
-    }
-    
-    function obdelajSporocilo(sporocilo){
-      
-
-      var array = sporocilo.split(" ");
-      var arraySlik = [];
-      console.log("sporocilo: "+sporocilo);
-      for(var i = 0; i < array.length; i++){
-        if(jeSlika(array[i])){
-          console.log("to je slika: "+array[i]);
-          arraySlik[arraySlik.length] = array[i];
-        }  
-      }
-      $('#sporocila').append(divElementEnostavniTekst(sporocilo));
-      $('#sporocila').scrollTop($('#sporocila').prop('scrollHeight'));
-      
-      if(arraySlik){
-        for(var k = 0; k < arraySlik.length; k++){
-        	$('#sporocila').append(divElementDodajSliko(arraySlik[k]));
-      		$('#sporocila').scrollTop($('#sporocila').prop('scrollHeight'));
-        }
-      }
-      
-    }
-    
-    
-    function procesirajVnosUporabnika(klepetApp, socket) {
-      var sporocilo = $('#poslji-sporocilo').val();
-      sporocilo = dodajSmeske(sporocilo);
-      var sistemskoSporocilo;
-      
-      if (sporocilo.charAt(0) == '/') {
-        sistemskoSporocilo = klepetApp.procesirajUkaz(sporocilo);
-        if (sistemskoSporocilo) {
-          $('#sporocila').append(divElementHtmlTekst(sistemskoSporocilo));
-        }
-        $('#poslji-sporocilo').val('');
-        return;
-      }
-      
+    } else {
       sporocilo = filtirirajVulgarneBesede(sporocilo);
       klepetApp.posljiSporocilo(trenutniKanal, sporocilo);
-      
-      obdelajSporocilo(sporocilo);
-
-      $('#poslji-sporocilo').val('');
+      $('#sporocila').append(divElementEnostavniTekst(sporocilo));
+      $('#sporocila').scrollTop($('#sporocila').prop('scrollHeight'));
     }
-    
-    var socket = io.connect();
-    var trenutniVzdevek = "", trenutniKanal = "";
-    
-    var vulgarneBesede = [];
-    $.get('/swearWords.txt', function(podatki) {
-      vulgarneBesede = podatki.split('\r\n');
-    });
-    
-    function filtirirajVulgarneBesede(vhod) {
-      for (var i in vulgarneBesede) {
-        vhod = vhod.replace(new RegExp('\\b' + vulgarneBesede[i] + '\\b', 'gi'), function() {
-          var zamenjava = "";
-          for (var j=0; j < vulgarneBesede[i].length; j++)
-            zamenjava = zamenjava + "*";
-          return zamenjava;
-        });
-      }
-      return vhod;
-    }
-    
-    $(document).ready(function() {
-      var klepetApp = new Klepet(socket);
-    
-      socket.on('vzdevekSpremembaOdgovor', function(rezultat) {
-        var sporocilo;
-        if (rezultat.uspesno) {
-          trenutniVzdevek = rezultat.vzdevek;
-          $('#kanal').text(trenutniVzdevek + " @ " + trenutniKanal);
-          sporocilo = 'Prijavljen si kot ' + rezultat.vzdevek + '.';
-        } else {
-          sporocilo = rezultat.sporocilo;
-        }
-        $('#sporocila').append(divElementHtmlTekst(sporocilo));
+  
+    $('#poslji-sporocilo').val('');
+  }
+  
+  var socket = io.connect();
+  var trenutniVzdevek = "", trenutniKanal = "";
+  
+  var vulgarneBesede = [];
+  $.get('/swearWords.txt', function(podatki) {
+    vulgarneBesede = podatki.split('\r\n');
+  });
+  
+  function filtirirajVulgarneBesede(vhod) {
+    for (var i in vulgarneBesede) {
+      vhod = vhod.replace(new RegExp('\\b' + vulgarneBesede[i] + '\\b', 'gi'), function() {
+        var zamenjava = "";
+        for (var j=0; j < vulgarneBesede[i].length; j++)
+          zamenjava = zamenjava + "*";
+        return zamenjava;
       });
-    
-      socket.on('pridruzitevOdgovor', function(rezultat) {
-        trenutniKanal = rezultat.kanal;
+    }
+    return vhod;
+  }
+  
+  $(document).ready(function() {
+    var klepetApp = new Klepet(socket);
+  
+    socket.on('vzdevekSpremembaOdgovor', function(rezultat) {
+      var sporocilo;
+      if (rezultat.uspesno) {
+        trenutniVzdevek = rezultat.vzdevek;
         $('#kanal').text(trenutniVzdevek + " @ " + trenutniKanal);
-        $('#sporocila').append(divElementHtmlTekst('Sprememba kanala.'));
-      });
-    
-      socket.on('sporocilo', function (sporocilo) {
-        var strBesedilo = sporocilo.besedilo;
-        obdelajSporocilo(strBesedilo);
-        
-        
-        
-        
-        
-      });
-      
-    socket.on('kanali', function(kanali) {
-        $('#seznam-kanalov').empty();
-    
-        for(var kanal in kanali) {
-          kanal = kanal.substring(1, kanal.length);
-          if (kanal != '') {
-            $('#seznam-kanalov').append(divElementEnostavniTekst(kanal));
-          }
-        }
-    
-        $('#seznam-kanalov div').click(function() {
-          klepetApp.procesirajUkaz('/pridruzitev ' + $(this).text());
-          $('#poslji-sporocilo').focus();
-        });
+        sporocilo = 'Prijavljen si kot ' + rezultat.vzdevek + '.';
+      } else {
+        sporocilo = rezultat.sporocilo;
+      }
+      $('#sporocila').append(divElementHtmlTekst(sporocilo));
     });
-    
-    socket.on('uporabniki', function(uporabniki) {
-    	$('#seznam-uporabnikov').empty();
-    
-    	for (var i=0; i < uporabniki.length; i++) {
-      		$('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
-    	}
-      		$('#seznam-uporabnikov div').click(function(){
-    			$('#poslji-sporocilo').attr('value',('/zasebno "'+ $(this).text()+'" '));
-    			$('#poslji-sporocilo').focus();
-    		});
-  	});
+  
+    socket.on('pridruzitevOdgovor', function(rezultat) {
+      trenutniKanal = rezultat.kanal;
+      $('#kanal').text(trenutniVzdevek + " @ " + trenutniKanal);
+      $('#sporocila').append(divElementHtmlTekst('Sprememba kanala.'));
+    });
 
-		socket.on('dregljaj', function(receiver){
-      		console.log("Client-receiver: dregljaj je prispel\n---"+receiver.dregljaj);
-	      //inicializacija jrumble nad selektorjem vsebina
-	      $('#vsebina').jrumble();
-	      //zacetek tresenja
-	      $('#vsebina').trigger('startRumble');
-	      //po 5 sekundah se tresenje konèa
-	      setTimeout(function(){
-	        console.log("JRUMBLE se je zdaj ustavil");
-	        $('#vsebina').trigger('stopRumble');
-	      }, 1500);
-	      
-	    });
+
+  socket.on('uporabniki', function(uporabniki) {
+    $('#seznam-uporabnikov').empty();
     
-      setInterval(function() {
-        socket.emit('kanali');
-        socket.emit('uporabniki', {kanal: trenutniKanal});
-      }, 1000);
-    
-      $('#poslji-sporocilo').focus();
-    
-      $('#poslji-obrazec').submit(function() {
-        procesirajVnosUporabnika(klepetApp, socket);
-        return false;
-      });
-      
-      
+    for (var i=0; i < uporabniki.length; i++) {
+      $('#seznam-uporabnikov').append(divElementEnostavniTekst(uporabniki[i]));
+    }
+      $('#seznam-uporabnikov div').click(function(){
+    	$('#poslji-sporocilo').attr('value',('/zasebno "'+ $(this).text()+'" '));
+    	$('#poslji-sporocilo').focus();
+    });
+  });
+
+    socket.on('sporocilo', function (sporocilo) {
+      var novElement = divElementEnostavniTekst(sporocilo.besedilo);
+      $('#sporocila').append(novElement);
     });
     
-    function dodajSmeske(vhodnoBesedilo) {
-      var preslikovalnaTabela = {
-        ";)": "wink.png",
-        ":)": "smiley.png",
-        "(y)": "like.png",
-        ":*": "kiss.png",
-        ":(": "sad.png"
+    socket.on('kanali', function(kanali) {
+      $('#seznam-kanalov').empty();
+  
+      for(var kanal in kanali) {
+        kanal = kanal.substring(1, kanal.length);
+        if (kanal != '') {
+          $('#seznam-kanalov').append(divElementEnostavniTekst(kanal));
+        }
       }
-      for (var smesko in preslikovalnaTabela) {
-        vhodnoBesedilo = vhodnoBesedilo.replace(smesko,
-          "<img src='http://sandbox.lavbic.net/teaching/OIS/gradivo/" +
-          preslikovalnaTabela[smesko] + "' />");
-      }
-      return vhodnoBesedilo;
+  
+      $('#seznam-kanalov div').click(function() {
+        klepetApp.procesirajUkaz('/pridruzitev ' + $(this).text());
+        $('#poslji-sporocilo').focus();
+      });
+    });
+  
+    
+    //socket ki posluša dregljaje
+    socket.on('dregljaj', function(receiver){
+      console.log("Client-receiver: dregljaj je prispel\n---"+receiver.dregljaj);
+      //inicializacija jrumble nad selektorjem vsebina
+      $('#vsebina').jrumble();
+      //zacetek tresenja
+      $('#vsebina').trigger('startRumble');
+      //po 5 sekundah se tresenje konča
+      setTimeout(function(){
+        console.log("JRUMBLE se je zdaj ustavil");
+        $('#vsebina').trigger('stopRumble');
+      }, 1500);
+      
+    });
+  
+    setInterval(function() {
+      socket.emit('kanali');
+      socket.emit('uporabniki', {kanal: trenutniKanal});
+    }, 1000);
+  
+    $('#poslji-sporocilo').focus();
+  
+    $('#poslji-obrazec').submit(function() {
+      procesirajVnosUporabnika(klepetApp, socket);
+      return false;
+    });
+    
+    
+  });
+  
+  function dodajSmeske(vhodnoBesedilo) {
+    var preslikovalnaTabela = {
+      ";)": "wink.png",
+      ":)": "smiley.png",
+      "(y)": "like.png",
+      ":*": "kiss.png",
+      ":(": "sad.png"
     }
+    for (var smesko in preslikovalnaTabela) {
+      vhodnoBesedilo = vhodnoBesedilo.replace(smesko,
+        "<img src='http://sandbox.lavbic.net/teaching/OIS/gradivo/" +
+        preslikovalnaTabela[smesko] + "' />");
+    }
+    return vhodnoBesedilo;
+  }
